6d2c618a7900a8542aff44634698c0901e69eed4
Connor Potebnya <cpotebnya@gmail.com>
2019-06-01 04:33:11 -0700

    merge

--- client/src/components/Update.js
+++ client/src/components/Update.js
@@ -25,7 +25,7 @@ class Update extends React.Component {
     render() {
         return (
         <div>
-            <h4>Currently updated to patch {this.state.patch ? this.state.patch : ''}</h4>
+            <h4>Currently updated to patch: {this.state.patch ? this.state.patch : ''}</h4>
         </div>
         )
     }
